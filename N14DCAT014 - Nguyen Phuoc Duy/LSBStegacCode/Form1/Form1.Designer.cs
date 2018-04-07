ddb54102c54aa74ad713f7279cd3b46ff02ec929
jakenguyen1206 <nguyenphuocduy1212@gmail.com>
2018-04-07 15:00:52 +0700

    Add files via upload

---
+++ N14DCAT014 - Nguyen Phuoc Duy/LSBStegacCode/Form1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonEncode = new System.Windows.Forms.Button();
+            this.buttonDecode = new System.Windows.Forms.Button();
+            this.labelMess = new System.Windows.Forms.Label();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(71, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(264, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(71, 193);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 1;
+            this.buttonOpen.Text = "Open File";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonEncode
+            // 
+            this.buttonEncode.Location = new System.Drawing.Point(117, 290);
+            this.buttonEncode.Name = "buttonEncode";
+            this.buttonEncode.Size = new System.Drawing.Size(75, 23);
+            this.buttonEncode.TabIndex = 2;
+            this.buttonEncode.Text = "Encode";
+            this.buttonEncode.UseVisualStyleBackColor = true;
+            this.buttonEncode.Click += new System.EventHandler(this.buttonEncode_Click);
+            // 
+            // buttonDecode
+            // 
+            this.buttonDecode.Location = new System.Drawing.Point(209, 290);
+            this.buttonDecode.Name = "buttonDecode";
+            this.buttonDecode.Size = new System.Drawing.Size(75, 23);
+            this.buttonDecode.TabIndex = 3;
+            this.buttonDecode.Text = "Decode";
+            this.buttonDecode.UseVisualStyleBackColor = true;
+            this.buttonDecode.Click += new System.EventHandler(this.buttonDecode_Click);
+            // 
+            // labelMess
+            // 
+            this.labelMess.AutoSize = true;
+            this.labelMess.Location = new System.Drawing.Point(79, 243);
+            this.labelMess.Name = "labelMess";
+            this.labelMess.Size = new System.Drawing.Size(78, 13);
+            this.labelMess.TabIndex = 4;
+            this.labelMess.Text = "Enter Message";
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Location = new System.Drawing.Point(167, 195);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.ReadOnly = true;
+            this.textBoxFilePath.Size = new System.Drawing.Size(168, 20);
+            this.textBoxFilePath.TabIndex = 5;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(167, 240);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(168, 20);
+            this.textBoxMessage.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 334);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.textBoxFilePath);
+            this.Controls.Add(this.labelMess);
+            this.Controls.Add(this.buttonDecode);
+            this.Controls.Add(this.buttonEncode);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LSB STEG - Author: N14DCAT014";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonEncode;
+        private System.Windows.Forms.Button buttonDecode;
+        private System.Windows.Forms.Label labelMess;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.TextBox textBoxMessage;
+    }
+}
+
---
+++ N14DCAT014 - Nguyen Phuoc Duy/LSBStegacCode/Form1/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Oepn File Button
+        public void buttonOpen_Click(object sender, EventArgs e)
+        {
+            //Mở File
+            OpenFileDialog openDialog = new OpenFileDialog();
+            //Lọc File
+            openDialog.Filter = "Image Files (*.png) | *.png";
+            //Default Path
+            openDialog.InitialDirectory = @"C:\";
+            //Ghi File Path vào Textbox
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFilePath.Text = openDialog.FileName.ToString();
+                pictureBox1.ImageLocation = textBoxFilePath.Text;
+            }
+        }
+        //padding bit 0 nếu không đủ 8 bit
+        public string padding(int b)
+        {
+            //Đổi sang hệ Bin dạng String
+            string bin = Convert.ToString(b, 2);
+            //Console.WriteLine("bin before: " + bin);
+            while (bin.Length < 8)
+            {
+                bin = "0" + bin;
+                //Console.WriteLine("bining: " + bin);
+            }
+            //Console.WriteLine("bin after: " + bin);
+            return bin;
+        }
+        //Chuyển Chuỗi thành Binary
+        public string MessageToBinary(string msg)
+        {
+            string result = "";
+
+            for (int i = 0; i < msg.ToCharArray().Length; i++)
+            {
+                char c = msg.ToCharArray()[i];
+                result += padding(c);
+            }
+            return result;
+        }
+        //Chuyển chuỗi Binary thành Byte - để kết hợp với Hàm Encode,ASCII
+        public static Byte[] GetBytesFromBinaryString(string binary)
+        {
+            Console.WriteLine("byte" + binary);
+            var list = new List<Byte>();
+
+            for (int i = 0; i < binary.Length-1; i += 8)
+            {
+                string t = binary.Substring(i, 8);
+
+                list.Add(Convert.ToByte(t, 2));
+            }
+
+            return list.ToArray();
+        }
+
+        //Button Encode
+        public void buttonEncode_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            //Chuyển Message thành Bin dạng String
+            string msgInBin = MessageToBinary(textBoxMessage.Text);
+            //Thêm độ dài message (Hệ bin) vào đầu Chuỗi Bin
+            string hiddenString = padding(textBoxMessage.Text.Length) + msgInBin;
+            //Thêm vài số 0 đăng sau để độ dài chuỗi chia hết cho 3 vì Pixel có 3 giá trị R G B
+            while (hiddenString.Length % 3 != 0)
+            {
+                hiddenString += "0";
+            }
+            //Chuyển thành Mảng Char
+            char[] binHidden = hiddenString.ToCharArray();
+            //Console.WriteLine(binHidden);
+            //Lấy Ảnh
+            Bitmap img = new Bitmap(textBoxFilePath.Text);
+            //Lấy từng Pixel
+            for (int row = 0; row < img.Width; row++)
+            {
+                for (int column = 0; column < img.Height; column++)
+                {
+                    if (count == binHidden.Length)
+                        break;
+                    Color pixel = img.GetPixel(row, column);
+                    //Console.WriteLine("old pixel: " + pixel);
+                    int newPixelR = (binHidden[count++] == '0') ? (pixel.R & 254) : (pixel.R | 1);
+                    int newPixelG = (binHidden[count++] == '0') ? (pixel.G & 254) : (pixel.G | 1);
+                    int newPixelB = (binHidden[count++] == '0') ? (pixel.B & 254) : (pixel.B | 1);
+                    img.SetPixel(row, column, Color.FromArgb(newPixelR, newPixelG, newPixelB));
+                    //Console.WriteLine("Color: " + newPixelR + " " + newPixelG + " " + newPixelB);
+                    //Console.WriteLine("new pixel: " + img.GetPixel(row, column));
+                }
+                if (count == binHidden.Length)
+                    break;
+            }
+
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Image Files (*.png) | *.png";
+            saveFile.InitialDirectory = @"C:\";
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFilePath.Text = saveFile.FileName.ToString();
+                pictureBox1.ImageLocation = textBoxFilePath.Text;
+
+                img.Save(textBoxFilePath.Text);
+                textBoxMessage.Clear();         //Xóa Message
+            }
+        }
+
+        //Lấy LSB trong Pixel
+        public static string getLSBBits(Color pixel)
+        {
+            string result = "";
+            string redInPixel = Convert.ToString(pixel.R, 2);
+            string greenInPixel = Convert.ToString(pixel.G, 2);
+            string blueInPixel = Convert.ToString(pixel.B, 2);
+            //Console.WriteLine("Pixel " + redInPixel + " " + greenInPixel + " " + blueInPixel);
+            result += redInPixel[redInPixel.Length - 1];
+            result += greenInPixel[greenInPixel.Length - 1];
+            result += blueInPixel[blueInPixel.Length - 1];
+
+            return result;
+        }
+        //Button Decode
+        public void buttonDecode_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(textBoxFilePath.Text);
+            var message = "";
+            int count = 0;
+            int length = 0;
+
+            for (int row = 0; row < img.Width; row++)
+            {
+                for (int column = 0; column < img.Height; column++)
+                {
+                    Color pixel = img.GetPixel(row, column);
+                    //Console.WriteLine("Pixel get: " + pixel);
+                    message += getLSBBits(pixel);
+                    count += 3;        //Vì lấy 1 pixel thì ta sẽ được 3 LSB (R - G - B)
+                    //Console.WriteLine("message" + message);
+                    //Console.WriteLine("count" + count);
+                    if (count == 9)
+                    {
+                        length = Convert.ToInt32(message.Substring(0, 8), 2); //Chuyển từ String sang Bin
+                        //Console.WriteLine(length+1);
+                        message = message.Substring(8); //Message sẽ từ vị trí thứ 8 trở đi (8 bit đầu là độ dài chuỗi)
+                    }
+                    if (count >= ((length+1) * 8)) //Length + 1 vì tính luôn 8 bit đầu của length
+                    {
+                        int du = count % ((length + 1) * 8);  //Phần dư, vì count + một lúc 3 đơn vị => sẽ có lúc vượt hơn length
+                                                             // VD: 19 ký tự => length = 160 còn count = 162
+                        message = message.Substring(0, message.Length - du);
+                        break;
+                    }
+
+                }
+                if (count >= ((length+1) * 8))
+                        break;
+            }
+            var data2 = GetBytesFromBinaryString(message);
+            message = Encoding.ASCII.GetString(data2);
+            textBoxMessage.Text = message;
+        }
+    }
+}
